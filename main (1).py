b7800903be25308caec42dc4f379e3e1b28405bf
8428701436 <144314196+8428701436@users.noreply.github.com>
2023-10-10 11:02:44 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,13 +1,24 @@
-# importing the module
-import calendar
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input. The function should perform a linear search to find the target product in the list and
+return a list of indices of all occurrences of the product if found, or an empty list if the product is not
+found.
+"""
 
-# input the year 
-year=int(input('Enter the value of year: '))
-leap_year=calendar.isleap(year)
 
-# checking leap year
-if leap_year: # to check condition
-    print('The given year is a leap year.')
-else:
-    print('The given year is a non-leap year.')
-  
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
